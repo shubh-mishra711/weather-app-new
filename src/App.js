5750ef0d7c45fb3904aa76bf0fc6129b53b0667d
shubh-mishra711 <mishrashubh1107@gmail.com>
2023-08-08 10:05:08 +0530

    monitoring

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 import Weather from './components/Weather';
 import CurrencyConverter from './components/cc';
 // import CurrencyConverter from './components/CurrencyConverter';
+import { init as initApm } from '@elastic/apm-rum'
+
+
+initApm({
+
+  // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
+  serviceName: 'weatherapp',
+
+  serverUrl: 'https://a87c533a6a144bbe9dd0931d52cd0fe5.apm.us-central1.gcp.cloud.es.io:443',
+
+  serviceVersion: '',
+
+  environment: 'my-app-environment',
+
+  logLevel: "debug"
+})
 
 function App() {
   return (
